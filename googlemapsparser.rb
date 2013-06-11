8d7fe3817f861272083d3331dfb51d8c0c002ee7
theoretick <mail@limitlesschannels.com>
2013-06-10 21:50:53 -0700

    cleaned up google maps parser

--- googlemapsparser.rb
+++ googlemapsparser.rb
@@ -16,15 +16,9 @@ class Zipcode
   end
 end
 
-# zips = Zipcode.new
-# zips.distance_to_from(11206, 95032)
-
 class Zip_Test < MiniTest::Unit::Test
   def test_distance_11206_to_95032
     assert_equal 11111, Zipcode.distance_to_from(11206, 95032)
   end
 end
 
-# class GoogleMaps
-#   def initialize()
-# end
